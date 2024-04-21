80bb2e872939d5767f29f2cc3b9fb9334bee583a
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-04-22 00:03:22 +0700

    navigation

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,80 +1,57 @@
-import ArtistPage from "../pages/ArtistPage/artistPage";
-import HomePage from "../pages/HomePage/HomePage";
-import artistDetailPage from "../pages/ArtistDetailPage/artistDetailPage";
-import NotFoundPage from "../pages/NotFoundPage/notFoundPage";
-import albumPage from "../pages/AbumPage/albumPage";
-import launchingPage from "../pages/LaunchingPage/launchingPage";
-import addToPlaylistPage from "../pages/AddToPlaylistPage/addToPlaylistPage";
-import exploreSearchPage from "../pages/ExploreSearchPage/exploreSearchPage";
-import likedSongsPage from "../pages/LikedSongsPage/likedSongsPage";
-import userProfilePage from "../pages/userProfile/userProfilePage";
-import LoginPage from "../pages/LoginPage/loginPage";
-import registerPage from "../pages/RegisterPage/registerPage";
-import playlistPage from "../pages/PlaylistPage/playlistPage";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export const routes = [
-  {
-    path: "/",
-    page: HomePage,
-    isShowHeader: true,
-  },
-  {
-    path: "/playlist",
-    page: playlistPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/artist",
-    page: ArtistPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/artist/:id",
-    page: artistDetailPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/album",
-    page: albumPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/launching",
-    page: launchingPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/add-to-playlist",
-    page: addToPlaylistPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/explore-search",
-    page: exploreSearchPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/liked-songs",
-    page: likedSongsPage,
-    isShowHeader: false,
-  },
-  {
-    path: "/user-profile",
-    page: userProfilePage,
-    isShowHeader: false,
-  },
-  {
-    path: "/login",
-    page: LoginPage,
-    isShowHeader: true,
-  },
-  {
-    path: "/register",
-    page: registerPage,
-    isShowHeader: true,
-  },
-  {
-    path: "*",
-    page: NotFoundPage,
-  },
-];
+// import ArtistPage from "../pages/ArtistPage/artistPage";
+// import HomePage from "../pages/HomePage/HomePage";
+// import artistDetailPage from "../pages/ArtistDetailPage/artistDetailPage";
+// import NotFoundPage from "../pages/NotFoundPage/notFoundPage";
+// import albumPage from "../pages/AlbumPage/albumPage";
+// import launchingPage from "../pages/LaunchingPage/launchingPage";
+// import addToPlaylistPage from "../pages/AddToPlaylistPage/addToPlaylistPage";
+// import exploreSearchPage from "../pages/ExploreSearchPage/exploreSearchPage";
+// import likedSongsPage from "../pages/LikedSongsPage/likedSongsPage";
+// import userProfilePage from "../pages/userProfile/userProfilePage";
+// import LoginPage from "../pages/LoginPage/loginPage";
+// import registerPage from "../pages/RegisterPage/registerPage";
+// import playlistPage from "../pages/PlaylistPage/playlistPage";
+
+// const Stack = createNativeStackNavigator();
+
+// const MyNavigationContainer = () => {
+//   return (
+//     <NavigationContainer independent={true}>
+//       <Stack.Navigator screenOptions={{ headerShown: false }}>
+//         <Stack.Screen name="SignIn" component={LoginPage} />
+
+//         {/* homepage */}
+//         <Stack.Screen name="HomePage" component={HomePage} />
+
+//         {/* artist  */}
+//         <Stack.Screen name="ArtistPage" component={ArtistPage} />
+
+//         <Stack.Screen name="artistDetailPage" component={artistDetailPage} />
+//         {/* user profile */}
+//         <Stack.Screen name="UserProfilePage" component={userProfilePage} />
+//         {/* register page */}
+//         <Stack.Screen name="RegisterPage" component={registerPage} />
+//         {/* login page */}
+//         <Stack.Screen name="LoginPage" component={LoginPage} />
+//         {/* launching page */}
+//         <Stack.Screen name="LaunchingPage" component={launchingPage} />
+//         {/* add to playlist page */}
+//         <Stack.Screen name="AddToPlaylistPage" component={addToPlaylistPage} />
+//         {/* explore search page */}
+//         <Stack.Screen name="ExploreSearchPage" component={exploreSearchPage} />
+//         {/* liked songs page */}
+//         <Stack.Screen name="LikedSongsPage" component={likedSongsPage} />
+//         {/* playlist page */}
+//         <Stack.Screen name="PlaylistPage" component={playlistPage} />
+//         {/* not found page */}
+//         <Stack.Screen name="NotFoundPage" component={NotFoundPage} />
+//         {/* album page */}
+//         <Stack.Screen name="AlbumPage" component={albumPage} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+// export default MyNavigationContainer;

455e8003ca66c7310244f4110b24ef86089413c6
tranuyn <127413543+tranuyn@users.noreply.github.com>
2024-05-26 04:44:07 +0700

    update Bottom Navigation Bar

--- client/components/MainContainer.js
+++ client/components/MainContainer.js
@@ -1,84 +1,79 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Ionicons from "react-native-vector-icons/Ionicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import HomePage from "../pages/HomePage/HomePage";
 import launchingPage from "../pages/LaunchingPage/launchingPage";
-import LibraryPage from "../pages/LibraryPage/LibraryPage";
+import LibraryPage from "../pages/FavoritePage/FavoritePage";
 import UserPage from "../pages/userProfilePage/userProfilePage";
 
 const homeName = "Home";
 const userName = "User";
 const launchingName = "Launching";
 const libraryName = "Library";
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const theme = {
+  colors: {
+    primaryContainer: '#737373',
+    secondaryContainer: 'transparent',
+  },
+};
+
 
 export default function MainContainer() {
-  const getTabScreenOptions = ({ route }) => ({
-    tabBarActiveTintColor: "#49A078",
-    tabBarLabelStyle: { fontSize: 12 },
-    tabBarStyle: {
-      backgroundColor: "#737373",
-      height: 50,
-    },
-    tabBarItemStyle: {
-      backgroundColor: "#737373",
-      height: 50,
-    },
-  });
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme} >
       <View style={styles.tabContainer}>
         <Tab.Navigator
           initialRouteName={homeName}
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
-              if (rn === homeName) {
-                iconName = focused ? "home" : "home-outline";
-              } else if (rn === launchingName) {
-                iconName = focused ? "file-find" : "file-find-outline";
-              } else if (rn === libraryName) {
-                iconName = focused ? "library" : "library-outline";
-              } else if (rn === userName) {
-                iconName = focused ? "person" : "person-outline";
-              }
-
-              if (
-                iconName === "file-find" ||
-                iconName === "file-find-outline"
-              ) {
-                return (
-                  <MaterialCommunityIcons
-                    name={iconName}
-                    size={size}
-                    color={color}
-                  />
-                );
-              } else {
-                return <Ionicons name={iconName} size={size} color={color} />;
-              }
-            },
-            tabBarActiveTintColor: "#49A078",
-            tabBarLabelStyle: { fontSize: 12 },
-            tabBarStyle: {
-              backgroundColor: "#737373",
-              height: 60,
-              paddingTop: 5,
-            },
-            tabBarItemStyle: {
-              height: 50,
-            },
-          })}
+          activeColor="#FED215"
+          inactiveColor="#979797"
+          barStyle= {{
+            backgroundColor: "#737373",
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            height: 80,
+            position: 'absolute',
+            overflow: 'hidden',
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+          shifting={true}
         >
-          <Tab.Screen name={homeName} component={HomePage} />
-          <Tab.Screen name={launchingName} component={launchingPage} />
-          <Tab.Screen name={libraryName} component={LibraryPage} />
-          <Tab.Screen name={userName} component={UserPage} />
+        
+          <Tab.Screen name={homeName} component={HomePage} 
+            options={{
+              tabBarLabel:  <Text style={styles.tabBarLabel}>Home</Text>,
+              tabBarIcon: ({ color, focused, }) => (
+                <Ionicons name={focused ? "home" : "home-outline"} color={color} size={30}/>
+              ),
+            }}
+          />
+          <Tab.Screen name={launchingName} component={launchingPage} 
+            options={{
+              tabBarLabel: <Text style={styles.tabBarLabel}>Explore</Text>,
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons name={focused ? "file-find" : "file-find-outline"} color={color} size={30} />
+              ),
+              
+            }}/>
+          <Tab.Screen name={libraryName} component={LibraryPage} 
+            options={{
+              tabBarLabel: <Text style={styles.tabBarLabel}>Favorite</Text>,
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialIcons name={focused ? "favorite" : "favorite-outline"} color={color} size={30} />
+              ),
+            }}/>
+          <Tab.Screen name={userName} component={UserPage} 
+            options={{
+              tabBarLabel: <Text style={styles.tabBarLabel}>User</Text>,
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? "person" : "person-outline"} color={color} size={30} />
+              )
+            }}/>
         </Tab.Navigator>
       </View>
     </NavigationContainer>
@@ -87,5 +82,9 @@ export default function MainContainer() {
 const styles = StyleSheet.create({
   tabContainer: {
     flex: 1,
+    backgroundColor: "transparent",
+  },
+  tabBarLabel: {
+    fontSize: 15,
   },
 });
